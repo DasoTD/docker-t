642252e5576697b230bed7fe4edf1cf4512508a2
DasoTD <daso.timileyin@gmail.com>
2021-05-30 16:40:30 -0700

    dd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) =>{
-    res.send("<H1>hello good day</H1>");
+    res.send("<H1>hello good day.</H1>");
 
 } );
 app.listen(PORT, () => console.log(`app is ruuning on port ${PORT}`));
